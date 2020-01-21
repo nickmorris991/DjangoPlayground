f49fecdfeb48df23dcbd333b57f20cb07bb3ede8
nickmorris991 <nickmorris25@outlook.com>
2020-01-21 18:30:32 -0500

    polls results.html added and some views

--- polls/urls.py
+++ polls/urls.py
@@ -5,16 +5,10 @@ from . import views
 # we can give path a view route (views.index) and the name of our view function (index)
 # and it will attach that view function to the route '' 
 # (which becuase is /polls because we're in that subdirectory)
+app_name = 'polls'
 urlpatterns = [
-    # ex: /polls/
     path('', views.index, name='index'),
-    # ex: /polls/5/
     path('<int:question_id>/', views.detail, name='detail'),
-    # ex: /polls/5/results/
     path('<int:question_id>/results/', views.results, name='results'),
-    # ex: /polls/5/vote/
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
-
-
-

944f9009a7fe79a43af7be97ed9790c450353a7d
nickmorris991 <nickmorris25@outlook.com>
2020-01-22 23:22:45 -0500

    finished django shorter views

--- polls/urls.py
+++ polls/urls.py
@@ -2,13 +2,10 @@ from django.urls import path
 
 from . import views
 
-# we can give path a view route (views.index) and the name of our view function (index)
-# and it will attach that view function to the route '' 
-# (which becuase is /polls because we're in that subdirectory)
 app_name = 'polls'
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<int:question_id>/', views.detail, name='detail'),
-    path('<int:question_id>/results/', views.results, name='results'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
